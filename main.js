f9fc79dbc17e9200d0b7f8c1bbf200c3c6375808
Derek Lobo <dereklobo89@gmail.com>
2022-07-25 08:39:30 -0400

    added variant

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 100,
             details: ['80% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 1, color: 'green'
+                },
+                {
+                    id: 2, color: 'blue'
+                }
+            ]
         }
     }
 })
